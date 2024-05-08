e6f7def36e35defc6b67813f4384bccd1f9d2baf
junyoung23 <163238619+junyoung23@users.noreply.github.com>
2024-05-08 23:10:42 +0900

    Update full_adder_tb.v

--- full_adder_tb.v
+++ full_adder_tb.v
@@ -6,11 +6,10 @@ module full_adder_tb;
 
     full_adder uut (.a(a), .b(b), .cin(cin), .sum(sum), .cout(cout));
 
+
     initial begin
         $monitor("Time = %0t: a = %b, b = %b, cin = %b | sum = %b, cout = %b",
                  $time, a, b, cin, sum, cout);
-
-        // 입력값 순환
         a = 0; b = 0; cin = 0; #10;
         a = 0; b = 0; cin = 1; #10;
         a = 0; b = 1; cin = 0; #10;
